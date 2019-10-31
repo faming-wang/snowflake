981e9c14a01b68f05369e61f4e9986a9892cffe5
Pat Wang <wang_faming@live.com>
2019-10-31 11:24:19 +0800

    Update and rename idworker.h to snowflake.h

--- snowflake.h
+++ snowflake.h
@@ -3,11 +3,11 @@
 #include <memory>
 #include <QtCore/qglobal.h>
 
-class IdWorker
+class Snowflake
 {
 public:
-    IdWorker(qint64 workerId, qint64 datacenterId, qint64 sequence = 0L);
-    ~IdWorker();
+    Snowflake(qint64 workerId, qint64 datacenterId, qint64 sequence = 0L);
+    ~Snowflake();
 
     qint64 nextId(); // Note: This function is thread-safe.
 
